bd6aac23718071bd31c5deb8aadbca9bf0b37935
Agnel Waghela <agnelwaghela@gmail.com>
2022-06-10 20:52:57 +0530

    Data Structure I Day 3 Array 350. Intersection of Two Arrays II (refactored)

--- study-plans/data-structures/data-structures-i/day-3-array-350-intersection_of_two_arrays_ii.rb
+++ study-plans/data-structures/data-structures-i/day-3-array-350-intersection_of_two_arrays_ii.rb
@@ -2,18 +2,14 @@
 # @param {Integer[]} nums2
 # @return {Integer[]}
 def intersect(nums1, nums2)
-  if nums1.size < nums2.size
-    left, right = nums1, nums2
-  else
-    left, right = nums2, nums1
-  end
+  nums1, nums2 = nums2, nums1 if nums1.size > nums2.size
   
   answer = []
-  left.size.times do |i|
-    idx_in_right = right.find_index(left[i])
-    if not idx_in_right.nil?
-      answer.push(left[i])
-      right.delete_at(idx_in_right)
+  nums1.size.times do |i|
+    idx_in_nums2 = nums2.find_index(nums1[i])
+    if not idx_in_nums2.nil?
+      answer.push(nums1[i])
+      nums2.delete_at(idx_in_nums2)
     end
   end
   

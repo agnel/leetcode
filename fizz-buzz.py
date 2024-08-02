913585ad4a55407f8d193519635f9ed97ef070b9
Agnel Waghela <agnel@users.noreply.github.com>
2024-08-02 15:43:55 +0530

    BCA Day 2: fizz-buzz.py
    This problem is part of assignment of BCA series.

---
+++ fizz-buzz.py
@@ -0,0 +1,17 @@
+# 412. Fizz Buzz
+# Ref: https://leetcode.com/problems/fizz-buzz/
+# tags: bca, day-2, assignment
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                answer.append("FizzBuzz")
+            elif i % 5 == 0:
+                answer.append("Buzz")
+            elif i % 3 == 0:
+                answer.append("Fizz")
+            else:
+                answer.append(str(i))
+
+        return answer

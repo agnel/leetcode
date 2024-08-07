468b45a27af318c0fb071cb5413c9f672719d387
Agnel Waghela <agnel@users.noreply.github.com>
2024-08-07 20:34:21 +0530

    refactor: convert-1d-array-into-2d-array.py
    This problem is part of assignment of BCA series.
    
    In this refactoring, the current solution seems to be better than the previous solution on leetcode.

--- convert-1d-array-into-2d-array.py
+++ convert-1d-array-into-2d-array.py
@@ -3,20 +3,13 @@
 # tags: bca, day-3, assignment
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        if len(original) != m * n:
+        size = len(original)
+        if size != m * n:
             return []
-    
-        result = [[0] * n for _ in range(m)]
-        
-        for i in range(len(original)):
-            # For  m=2,n=2
-            # 
-            # i      i/n         i%n        [ ][ ]
-            # 
-            # 0      0/2       0%2          [0][0]
-            # 1      1/2       1%2          [0][1]
-            # 2      2/2       2%2          [1][0]
-            # 3      3/2       3%2          [1][1]
-            result[i // n][i % n] = original[i]
-        
-        return result
+
+        res = []
+
+        for i in range(0, size, n):
+            res.append(original[i:i + n])
+
+        return res

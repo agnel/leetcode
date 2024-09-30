2bbfc0e8e9998b4dae388cce8cd8c5d5ce8c6a2c
Agnel Waghela <agnel@users.noreply.github.com>
2024-09-30 15:51:12 +0530

    BCA Day 6: refactor: search-in-rotated-sorted-array.py

--- search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array.py
@@ -2,43 +2,38 @@
 # Ref: https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 # tags: bca, day-6, searching, array, binary-search, medium, pivot
 class Solution:
-    # pivot means the index of the smallest element
-    # in an unrotated array it will be 0 i.e. the first element
-    def find_pivot(self, nums: List[int]) -> int:
-        l = 0 # low
-        r = len(nums) - 1 # high
-        while l < r:
-            # mid = low + (high - low) / 2
-            m = l + ((r - l) // 2)
-            if nums[m] < nums[r]:
-                r = m
-            else:
-                l = m + 1
-        
-        return l
-
-    def binary_search(self, nums: List[int], target: int, l: int, r: int) -> int:
-        while l <= r:
-            m = l + ((r - l) // 2)
-            if nums[m] < target:
-                l = m + 1
-            elif nums[m] > target:
-                r = m - 1
-            else:
-                return m
-        
-        return -1
-            
-
+    class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l = 0
-        r = len(nums) - 1
-        pivot = self.find_pivot(nums)
-        ans = -1
+        n = len(nums)
+        r = n - 1
 
-        if target >= nums[pivot] and target <= nums[r]:
-            ans = self.binary_search(nums, target, pivot, r)
+        # pivot means the index of the smallest element
+        # in an unrotated array it will be 0 i.e. the first element
+        while l < r:
+            mid = l + ((r - l) // 2)
+
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        pivot = l
+
+        # decide the range for searching out target
+        if pivot == 0:
+            l, r = 0, n - 1
+        elif target >= nums[pivot] and target <= nums[n - 1]:
+            l, r = pivot, n - 1
         else:
-            ans = self.binary_search(nums, target, l, pivot)
+            l, r = 0, pivot - 1
 
-        return ans
+        # use binary search for searching
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1

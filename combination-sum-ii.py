bc1dd7ee9c4bbad81fb289ea3ca32ccbbbb7ccb1
Agnel Waghela <agnel@users.noreply.github.com>
2024-10-17 13:05:28 +0530

    BCA Day 7: combination-sum-ii.py

---
+++ combination-sum-ii.py
@@ -0,0 +1,25 @@
+# 40. Combination Sum II
+# Ref: https://leetcode.com/problems/combination-sum-ii
+# Solution Ref: https://leetcode.com/problems/combination-sum-ii/editorial/
+# tags: bca, day-7, medium, array, backtracking, combination-sum-ii
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        candidates.sort() # in-place sorting
+        length = len(candidates)
+        
+        def backtrack(start, combination, target):
+            if target < 0:
+                return # backtrack immediately, since no solution found
+            if target == 0:
+                result.append(combination[:])
+                return # solution found
+            
+            for i in range(start, length):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                else:
+                    backtrack(i + 1, combination + [candidates[i]], target - candidates[i])
+        
+        backtrack(0, [], target)
+        return result
